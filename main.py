5347d9e07b1a303cf0cfafe72c10a5443759ea67
Rishit605 <rishit.sisodia2001@gmail.com>
2023-04-19 17:44:04 +0530

    Updatated

--- main.py
+++ main.py
@@ -1,53 +1,39 @@
 from flask import Flask, request, jsonify
-from flask_cors import CORS
-from tensorflow.keras.models import load_model
+
+import tensorflow as tf
 import numpy as np
-import pandas as pd
-from prediction import *
+from prediction import  convertPDFtoText, tok, classes
 
 app = Flask(__name__)
-CORS(app)
 
-# Load pre-trained deep learning model
-model = load_model('FirstTry.h5')
+# Load the pre-trained Keras model
+model = tf.keras.models.load_model('res_ana.h5')
 
-
-# Define preprocessing function
-def preprocess_text(text):
-    # Apply any preprocessing steps here, such as removing punctuation, stopwords, etc.
-    # Return the preprocessed text as a string
-    pdf_file = convertPDFtoText(text)
-    return pdf_file
+# Define the mapping of class indices to class names
+class_names = classes()
 
 
-# Define function to predict class from preprocessed text
-def predict_class(text):
-    # Convert text to vector using CountVectorizer or any other vectorizer that you prefer
-    # vector = count_vectorizer.transform([text])
-
-    tokened = tok(text)
-
-    # Make prediction using pre-trained model
-    prediction = model.predict(tokened)
-    # Convert prediction to class label
-    class_label = np.argmax(prediction)
-    # Return class label as a string
-    return str(class_label)
-
-
-# Define API route for file upload
 @app.route('/predict', methods=['POST'])
 def predict():
-    # Get uploaded file from request
+    # Get the uploaded file from the request
     file = request.files['file']
-    # Preprocess text data
-    data = preprocess_text(file)
-    # Make predictions for each text data point
-    preds = predict_class(data)
-    # Convert predicted class labels to list
-    # predicted_classes = data['predicted_class'].tolist()
-    # Return predicted classes as JSON response
-    return jsonify({'predicted_classes': preds})
+
+    # Convert the PDF file to text
+    text = convertPDFtoText(file)
+
+    # Preprocess the text
+    # ...
+    preprocessed_text = tok(text)
+    # Use the pre-trained model to make a prediction
+    # ...
+    prediction = model.predict(preprocessed_text)
+
+    # Convert the prediction to a class name
+    class_index = np.argmax(prediction)
+    class_name = class_names[class_index]
+
+    # Return the prediction as JSON
+    return jsonify({'class': class_name})
 
 
 if __name__ == '__main__':
