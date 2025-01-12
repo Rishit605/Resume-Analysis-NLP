48d0e86fdee62d092ff9c29026097d8ff9ec7158
Rishit605 <rishit.sisodia2001@gmail.com>
2025-01-12 23:27:07 +0530

    Update helpers.py
    Modified the Plotting function to save the plotted graph for monitoring or future insights.

--- src/utils/helpers.py
+++ src/utils/helpers.py
@@ -1,9 +1,13 @@
 import matplotlib.pyplot as plt
 
-def plot_accuracy(history, accuracy, val_accuracy):
+def plot_accuracy(history, accuracy, val_accuracy, save_path: bool = True):
     plt.plot(history.history[accuracy], label='Training Accuracy')
     plt.plot(history.history[val_accuracy], label='Validation Accuracy')
     plt.legend()
+
+    if save_path:
+        plt.savefig(r'C:\Projs\COde\ResAnalysis\Resume-Analysis-NLP\src\training\training_plots')
+    
     plt.show()
 
 # plot_accuracy(history)
